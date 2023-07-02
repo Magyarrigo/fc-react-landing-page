0384846defce73237da188a48504f9ff64f22d27
Boguslaw Drozd <b.drozd07@gmail.com>
2023-07-02 14:26:05 +0200

    offers square by map()

--- src/elements/Offer/Offer.js
+++ src/elements/Offer/Offer.js
@@ -1,22 +1,44 @@
 import Container from "../Container/Container";
 import "./Offer.css";
+
 import Square from "./Square/Square";
 
+const offerArray = [
+  {
+    offerName: "Offer one",
+    isNew: false,
+  },
+  {
+    offerName: "Offer two",
+    isNew: false,
+  },
+  {
+    offerName: "Offer three",
+    isNew: true,
+  },
+  {
+    offerName: "Offer four",
+    isNew: false,
+  },
+  {
+    offerName: "Offer fife",
+    isNew: false,
+  },
+  {
+    offerName: "Offer six",
+    isNew: true,
+  },
+];
+
 function Offer({ offerTitle }) {
-    
   return (
     <section className="offerContainer" id="ourOffer">
       <Container>
         <h1 className="ourOfferTitle">{offerTitle}</h1>
         <div className="ourOffersContainer">
-          
-            <Square offerName = "Usługa one" isNew={false}/>
-            <Square offerName = "Usługa second" isNew={true}/>
-            <Square offerName = "Usługa one" isNew={false}/>
-            <Square offerName = "Usługa one" isNew={false}/>
-            <Square offerName = "Usługa one" isNew={false}/>
-            <Square offerName = "Usługa second" isNew={true}/>
-          
+          {offerArray.map(function (item) {
+            return <Square offerName={item.offerName} isNew={item.isNew} />;
+          })}
         </div>
       </Container>
     </section>
--- src/elements/Offer/Square/Square.js
+++ src/elements/Offer/Square/Square.js
@@ -4,12 +4,10 @@ function Square({ offerName, isNew }) {
   if (isNew) {
     return (
       <div className="square squareNew">
-        <div className = "addNew">
-        <h2>{offerName}</h2>
-        <h4>(nowosc)</h4>
-
+        <div className="addNew">
+          <h2>{offerName}</h2>
+          <h4>(nowosc)</h4>
         </div>
-        
       </div>
     );
   } else {
